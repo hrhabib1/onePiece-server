c40ded60e2f49c61108b8ba57737b1fe407cba75
hrhabib1 <sayefhabib123@gmail.com>
2024-04-02 03:49:05 +0600

    create post

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -38,6 +38,17 @@ async function run() {
       app.get('/addSellPost', async(req, res) =>{
         const cursor = addSellPosts.find();
         const result = await cursor.toArray();
+        res.send(result);
+      })
+
+      app.get('/addSellPost/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const options = {
+            projection: {title: 1, image: 1,  priceKilo: 1, price: 1},
+        };
+
+        const result = await addSellPosts.findOne(query, options);
         res.send(result);
       })
 
